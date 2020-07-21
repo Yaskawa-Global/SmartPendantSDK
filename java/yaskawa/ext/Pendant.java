5738887fb3bdf1e6c92d52fce2c5ea64c68167a8
Leo Gonzalez <Leonel.Gonzalez@yaskawainnovation.com>
2020-07-21 14:18:49 -0500

    Revert "Merge branch 'Version2_0_x'"
    This reverts commit aba1ee37d55263a10125049f8b772ff7ec025bca, reversing
    changes made to b8cb4bcc1f83e9b1b26a682ccc14221ca60e90f3.

--- java/yaskawa/ext/Pendant.java
+++ java/yaskawa/ext/Pendant.java
@@ -196,11 +196,6 @@ public class Pendant
         client.cancelPopupDialog(id, identifier);
     }
 
-    public String insertInstructionAtSelectedLine(String instruction) throws TException
-    {
-        return client.insertInstructionAtSelectedLine(id, instruction);
-    }
-
 
     // Event consumer functions
 
