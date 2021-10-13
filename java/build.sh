3a3379634feb2da5c4350b73e51aa6b2dc2cad33
David Jung <david.jung@yaskawainnovation.com>
2021-10-13 16:04:32 -0500

    Added code to download dep jars from web to lib dir for client jar build

--- java/build.sh
+++ java/build.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
+
+# Download dependent libs first, if not already present locally
+mkdir -p lib
+cd lib
+if [ ! -f "libthrift-0.11.0.jar" ]; then
+  wget https://s3.us-east-2.amazonaws.com/yaskawa-yii/SmartPendant/extension/libthrift-0.11.0.jar
+fi
+if [ ! -f "slf4j-api.jar" ]; then
+  wget https://s3.us-east-2.amazonaws.com/yaskawa-yii/SmartPendant/extension/slf4j-api.jar
+fi
+# not actually needed for building the client code that uses the API, but may be used by extensions:
+if [ ! -f "slf4j-simple.jar" ]; then
+  wget https://s3.us-east-2.amazonaws.com/yaskawa-yii/SmartPendant/extension/slf4j-simple.jar
+fi
+cd ..
+
 cd gen-java
 javac -Xlint:deprecation -cp ../lib/libthrift-0.11.0.jar:../lib/slf4j-api.jar yaskawa/ext/api/*.java
 cd ..
