bc5be8ec8f95516c19e110d9ca9a26f5c30bd43d
David Jung <david.jung@yaskawainnovation.com>
2021-10-19 16:12:02 -0500

    Augmented DemoExtnesion21 build.sh to download jar libs

--- DemoExtension21/run.sh
+++ DemoExtension21/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp lib/yaskawa-ext-2.1.0.jar:lib/libthrift-0.11.0.jar:lib/slf4j-api.jar:lib/slf4j-simple.jar:DemoExtension.jar:. DemoExtension $1 $2
+java -cp java/lib/yaskawa-ext-2.2.0.jar:java/lib/libthrift-0.11.0.jar:java/lib/slf4j-api.jar:java/lib/slf4j-simple.jar:DemoExtension.jar:. DemoExtension $1 $2
 
--- java/build.sh
+++ java/build.sh
@@ -20,7 +20,7 @@ javac -Xlint:deprecation -cp ../lib/libthrift-0.11.0.jar:../lib/slf4j-api.jar ya
 cd ..
 javac -Xlint:deprecation -Xlint:unchecked -cp lib/libthrift-0.11.0.jar:lib/slf4j-api.jar:gen-java yaskawa/ext/*.java
 cd gen-java
-jar cf ../yaskawa-ext-2.1.0.jar yaskawa
+jar cf ../yaskawa-ext-2.2.0.jar yaskawa
 cd ..
-jar uf yaskawa-ext-2.1.0.jar yaskawa
+jar uf yaskawa-ext-2.2.0.jar yaskawa
 
