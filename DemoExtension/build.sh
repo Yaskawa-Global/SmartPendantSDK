9b2429a8e27030606af9f3f3d0fb43f6fedf7a83
Andrew Boddiford <andrew.boddiford@yaskawainnovation.com>
2021-11-23 14:46:43 -0600

    Simplify scripts and add batch files for Windows users

--- DemoExtension/build.sh
+++ DemoExtension/build.sh
@@ -1,4 +1,4 @@
 cd java
-javac -cp ../../../../External/thrift/lib/java/build/libthrift-0.11.0.jar:/usr/share/java/slf4j-api.jar:../../java/yaskawa-ext-2.0.4.jar *.java
+javac -cp ../dependencies/libthrift-0.11.0.jar:../dependencies/slf4j-api.jar:../dependencies/yaskawa-ext-2.0.4.jar *.java
 jar -cfe ../DemoExtension.jar DemoExtension DemoExtension.class
-
+cd ..
---
+++ DemoExtension/run.bat
@@ -0,0 +1,1 @@
+java -cp dependencies/yaskawa-ext-2.0.4.jar;dependencies/libthrift-0.11.0.jar;dependencies/slf4j-api.jar;dependencies/slf4j-simple.jar;DemoExtension.jar;. DemoExtension $1 $2
