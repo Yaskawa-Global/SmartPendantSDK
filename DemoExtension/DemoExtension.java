8bfc3b3a50c112f4d2c069021e22c5dd2b62232d
Baptiste Couland <baptiste.couland@yaskawainnovation.com>
2025-06-10 15:32:31 -0500

    Added missing import

--- DemoExtension/DemoExtension.java
+++ DemoExtension/DemoExtension.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 import java.net.Socket;
